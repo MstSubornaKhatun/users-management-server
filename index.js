98defcb7ac0759d4b3490a0b5df0e8b6c99774ab
suborna33 <152749142+suborna33@users.noreply.github.com>
2026-02-09 10:30:01 +0600

    server setup

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get('/', (req, res)=>{
+    res.send('User Management Server is running!')
+})
+
+const users = [
+    {id: 1, name: 'Sabana', email: 'sabana@gmail.com'},
+    {id: 2, name: 'Pabana', email: 'pabana@gmail.com'},
+    {id: 3, name: 'Thakbana', email: 'thakbana@gmail.com'},
+]
+
+app.get('/users', (req, res)=>{
+    res.send(users);
+})
+
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`)
+})
